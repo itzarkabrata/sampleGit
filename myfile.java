482e845a64275e270c0f97b0f803e63272927eac
Arkabrata Chandra <arkabrata1292@gmail.com>
2024-04-02 14:55:57 +0530

    myfile.java is commited

--- myfile.java
+++ myfile.java
@@ -2,6 +2,6 @@ package mygit;
 
 public class myfile {
     public static void main(String[] args) {
-        System.out.println("Helloo Welcome to Git");
+        System.out.println("Helloo Welcome to Git my friend Arkabrata");
     }  
 }
