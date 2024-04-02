222028a1a6adcbba3cb8842ab0a1e41beb37669e
Arkabrata Chandra <arkabrata1292@gmail.com>
2024-04-02 15:16:47 +0530

    multiply function embedded

---
+++ mul.java
@@ -0,0 +1,9 @@
+package mygit;
+
+public class mul {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        System.out.println("Multiplication : "+(a+b));
+    }
+}
